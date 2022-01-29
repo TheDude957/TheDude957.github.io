5af5d497b64faefe85a9d0977804aeb0d5a4ceed
Julian Lussier <julianlussier@gmail.com>
2022-01-29 17:29:20 -0500

    teset4

--- store_map.js
+++ store_map.js
@@ -1,2 +1,16 @@
-alert("hello");
 
+function get_map(){
+$.ajax({
+    type: "POST",
+    url: 'refreshUsersList.php',
+   dataType: 'json',
+    data: "check",
+    success: function(response){
+     
+      `worddsss`
+      
+    }
+});
+}
+document.write("other words");
+get_map();
