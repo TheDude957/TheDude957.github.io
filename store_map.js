43654cde264bf1b97297723b013e1ac71063bbc2
Julian Lussier <julianlussier@gmail.com>
2022-01-29 21:53:14 -0500

    order addresses implemented still a bug left

--- store_map.js
+++ store_map.js
@@ -11,36 +11,74 @@ $.ajax({
     }
 });
 }
-var createCORSRequest = function(method, url) {
-  var xhr = new XMLHttpRequest();
-  if ("withCredentials" in xhr) {
-    // Most browsers.
-    xhr.open(method, url, true);
-  } else if (typeof XDomainRequest != "undefined") {
-    // IE8 & IE9
-    xhr = new XDomainRequest();
-    xhr.open(method, url);
-  } else {
-    // CORS not supported.
-    xhr = null;
-  }
-  return xhr;
-};
 
-var url = 'https://sapstore.conuhacks.io/orders/246';
-var method = 'GET';
-var xhr = createCORSRequest(method, url);
+// getting store locations
+function getAddresses(xhr){
+  var customer_addresses = JSON.parse(xhr.response);
+  const addresses = [];
+   for(let i in customer_addresses){
+    address.push(customer_addresses.customerAddress);
+   }
+   return addresses
+}
 
-xhr.onload = function() {
-  // Success code goes here.
+// getting customer location
+function getAddress(xhr, email_or_ID){
+  
   var customer_address = JSON.parse(xhr.response);
-  document.write(customer_address.customerAddress)
-};
+  var address;
+  if (isNaN(email_or_ID))  
+    address = customer_address[0].customerAddress;
+  
+  else
+    address = customer_address.customerAddress;
 
-xhr.onerror = function() {
-  // Error code goes here.
-};
+  
+   return address;
 
-xhr.send();
+}
 
-//get_map();
+function makeRequest(order_or_store,email_OR_ID, myfunction){
+    if(isNaN(email_OR_ID)){
+      email_OR_ID = 'byEmail?email='+ email_OR_ID ;
+      document.write('https://sapstore.conuhacks.io/orders/' + email_OR_ID)
+    }
+  var createCORSRequest = function(method, url) {
+    var xhr = new XMLHttpRequest();
+    if ("withCredentials" in xhr) {
+      // Most browsers.
+      xhr.open(method, url, true);
+    } else if (typeof XDomainRequest != "undefined") {
+      // IE8 & IE9
+      xhr = new XDomainRequest();
+      xhr.open(method, url);
+    } else {
+      // CORS not supported.
+      xhr = null;
+    }
+    return xhr;
+  };
+
+    var url = 'https://sapstore.conuhacks.io/' + order_or_store + "/" + email_OR_ID;
+    console.log(url);
+    var method = 'GET';
+    var xhr = createCORSRequest(method, url);
+
+    xhr.onload = function() {
+      // Success code goes here.
+      console.log(xhr.responseText);
+      var customer_address = JSON.parse(xhr.responseText);
+
+      var thing = myfunction(xhr,email_OR_ID);
+      return thing;
+    };
+
+    xhr.onerror = function() {
+      return null;
+    };
+
+    xhr.send();
+}
+
+
+document.write(makeRequest("orders","246", getAddress));
